10da20e1253e273b273bb1fc2ded830ed5a62cad
Augusto Sousa <lagrange.tesla@gmail.com>
2022-05-22 12:16:27 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MauSousa/hellomod2
+
+go 1.18
---
+++ utils.go
@@ -0,0 +1,11 @@
+package utils
+
+import "fmt"
+
+func HelloWorld() {
+  fmt.Println("Hello World")
+}
+
+func PrintSomething(something string) {
+  fmt.Println(something)
+}
